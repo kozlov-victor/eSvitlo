012a9a18cf98dd36ad1a9ac21fbb1e916f469672
Kozlov Victor <kozlovvictorzx@gmail.com>
2026-02-22 18:56:00 +0200

    DEV-1: 2026.1.29

--- src/app/firmware_version.h
+++ src/app/firmware_version.h
@@ -3,7 +3,7 @@
 #define SVITLO_FIRMWARE_VERSION_H
 #include <Arduino.h>
 
-#define FIRMWARE_VERSION "FW_VERSION=2026.1.28"
+#define FIRMWARE_VERSION "FW_VERSION=2026.1.29"
 
 class FirmwareVersion {
 private:
